0efc084acbe87f91c81d8f8c2073ba6790c84c45
Blastonn <felipe.paulo07@hotmail.com>
2025-02-18 16:19:18 -0300

    Feat - Criar funcao de rodada
    Feita a logica do jogo para uma rodada.

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ function getHumanChoice(escolha){
     return escolha;
 }
 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === "pedra" && computerChoice === "Papel")  {
+        computerScore++;
+        return console.log("Voce perdeu papel vence pedra! " + computerScore);
+    }else if(humanChoice === "papel" && computerChoice === "Pedra"){
+        humanScore++;
+        return console.log("Voce ganhou papel vence pedra! " + humanScore);
+    }else if(humanChoice === "tesoura" && computerChoice === "Pedra"){
+        computerScore++;
+        return console.log("Voce perdeu pedra amassa tesoura ! " + computerScore);
+    }else if(humanChoice === "pedra" && computerChoice === "Tesoura"){
+        humanScore++;
+        return console.log("Voce ganhou pedra amassa tesoura" + humanScore);
+    }else if(humanChoice === "tesoura" && computerChoice === "Papel"){
+        humanScore++;
+        return console.log("Voce ganhou tesoura corta papel" + humanScore);
+    }else if(humanChoice === "papel" && computerChoice === "Tesoura"){
+        computerScore++;
+        return console.log("Voce perdeu tesoura corta papel" + computerScore);
+    }else{
+        return console.log("Empate");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);

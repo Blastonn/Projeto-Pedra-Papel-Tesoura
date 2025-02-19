eb27b0d46159bd4d001d20b2558138f27acf5989
Blastonn <felipe.paulo07@hotmail.com>
2025-02-19 12:32:22 -0300

    Feat - Criar funcao para chamar 5 rounds
    Criada funcao playGame para chamar 5 rounds.

--- script.js
+++ script.js
@@ -20,34 +20,33 @@ function getHumanChoice(escolha){
     escolha = prompt("Pedra, papel ou tesoura?");
     return escolha;
 }
-
-function playRound(humanChoice, computerChoice){
-    
-    if (humanChoice.toLowerCase() === "pedra" && computerChoice === "Papel")  {
-        computerScore++;
-        return console.log("Voce perdeu papel vence pedra! " + computerScore);
-    }else if(humanChoice.toLowerCase() === "papel" && computerChoice === "Pedra"){
-        humanScore++;
-        return console.log("Voce ganhou papel vence pedra! " + humanScore);
-    }else if(humanChoice.toLowerCase() === "tesoura" && computerChoice === "Pedra"){
-        computerScore++;
-        return console.log("Voce perdeu pedra amassa tesoura ! " + computerScore);
-    }else if(humanChoice.toLowerCase() === "pedra" && computerChoice === "Tesoura"){
-        humanScore++;
-        return console.log("Voce ganhou pedra amassa tesoura" + humanScore);
-    }else if(humanChoice.toLowerCase() === "tesoura" && computerChoice === "Papel"){
-        humanScore++;
-        return console.log("Voce ganhou tesoura corta papel" + humanScore);
-    }else if(humanChoice.toLowerCase() === "papel" && computerChoice === "Tesoura"){
-        computerScore++;
-        return console.log("Voce perdeu tesoura corta papel" + computerScore);
-    }else{
-        return console.log("Empate");
-    }
+for (let i = 1; i <= 5; i++){
+function playGame(humanChoice, computerChoice){
+    let pontuaRobo = 0;
+    let pontuaHumano = 0;
+        function playRound(){ 
+            if (humanChoice.toLowerCase() === "pedra" && computerChoice === "Papel"){
+                pontuaRobo += 1;
+                return console.log("Voce perdeu papel vence pedra!  ");
+            }else if(humanChoice.toLowerCase() === "papel" && computerChoice === "Pedra"){
+                return console.log("Voce ganhou papel vence pedra! " );
+            }else if(humanChoice.toLowerCase() === "tesoura" && computerChoice === "Pedra"){
+                return console.log("Voce perdeu pedra amassa tesoura ! " );
+            }else if(humanChoice.toLowerCase() === "pedra" && computerChoice === "Tesoura"){
+                return console.log("Voce ganhou pedra amassa tesoura" );
+            }else if(humanChoice.toLowerCase() === "tesoura" && computerChoice === "Papel"){
+                return console.log("Voce ganhou tesoura corta papel" );
+            }else if(humanChoice.toLowerCase() === "papel" && computerChoice === "Tesoura"){
+                return console.log("Voce perdeu tesoura corta papel" );
+            }else{
+                console.log("empate");
+            }
+        }
+        return playRound();
 }
 
+    let choiceHuman = getHumanChoice();
+    let choiceComputer = getComputerChoice();
+    playGame(choiceHuman,choiceComputer);
+}
 
-const choiceHuman = getHumanChoice();
-const choiceComputer = getComputerChoice();
-
-playRound(choiceHuman,choiceComputer);
